f0810cdaa01d0ca5fb001286dfa0efc0e692cc71
Mirkonator <82175315+Mirkonator@users.noreply.github.com>
2026-01-19 17:57:34 +0100

    Add registry auth option to docker deploy command (#43)

--- deploy.go
+++ deploy.go
@@ -17,7 +17,7 @@ func main() {
 func deployDir(stackName string) error {
 	fmt.Printf("Deploying stack '%s'\n", stackName)
 
-	cmd := exec.Command("docker", "stack", "deploy", "-d", "--compose-file", "docker-compose.yaml", stackName)
+	cmd := exec.Command("docker", "stack", "deploy", "-d", "--compose-file", "docker-compose.yaml", stackName, "--with-registry-auth")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
